09b77a94b5106d611640c6b8735d2cf619d1d506
maciek trela <t.maciek@gmail.com>
2016-07-28 22:14:34 +0200

    test

--- src/main/java/Resource.java
+++ src/main/java/Resource.java
@@ -1,6 +1,15 @@
+import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import java.util.Arrays;
+import java.util.function.Consumer;
 
 /**
  * Created by maciek on 28.07.2016.
@@ -10,6 +19,24 @@ public class Resource {
 
     @GET
     public String hello(@PathParam("/{name}" )String name){
-        return name;
+        final String mongodb_user = System.getProperty("MONGODB_USER");
+        final String mongodb_password = System.getProperty("MONGODB_PASSWORD");
+        final String mongodb_database = System.getProperty("MONGODB_DATABASE");
+
+
+        MongoCredential credential = MongoCredential.createCredential(mongodb_user, mongodb_database, mongodb_password.toCharArray());
+        MongoClient mongoClient = new MongoClient(new ServerAddress(), Arrays.asList(credential));
+        MongoDatabase db = mongoClient.getDatabase(mongodb_database);
+
+        db.getCollection("items").insertOne(
+                new Document("name",name));
+
+
+        FindIterable<Document> iterable = db.getCollection("items").find();
+        StringBuilder names = new StringBuilder();
+        for (Document document : iterable) {
+            names.append(document.get("name"));
+        }
+        return names.toString();
     }
 }
