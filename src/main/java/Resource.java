b9cd48fe7f845dc2ad8334afa38dde95d5a110a5
maciek trela <t.maciek@gmail.com>
2016-07-28 22:14:45 +0200

    test

--- src/main/java/Resource.java
+++ src/main/java/Resource.java
@@ -9,7 +9,6 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import java.util.Arrays;
-import java.util.function.Consumer;
 
 /**
  * Created by maciek on 28.07.2016.
