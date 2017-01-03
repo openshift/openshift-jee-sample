609c7f7bc3aa8c68444cfaa393cb3eb004734e73
Waldo Mateo <wmateo@uoc.edu>
2017-01-03 11:00:59 +0100

    spring boot + angular + jpa (hsqldb)

---
+++ src/main/java/edu/uoc/demo/Application.java
@@ -0,0 +1,18 @@
+package edu.uoc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/**
+ * Created by wmateo on 03/01/2017.
+ */
+@SpringBootApplication
+@SuppressWarnings("squid:S2095")
+public class Application extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
